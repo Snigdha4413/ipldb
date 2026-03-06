7f3c78044f67ae9ef97cd29258a04d8bf9c715b0
Snigdha4413 <121025400+Snigdha4413@users.noreply.github.com>
2026-03-06 12:01:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request
+from sqlalchemy import create_engine
+
+app = Flask(__name__)
+
+DATABASE_URL = "your render external url"
+
+engine = create_engine(DATABASE_URL)
+
+@app.route('/')
+def home():
+    country = request.args.get('country')
+
+    conn = engine.connect()
+
+    if country:
+        query = f"SELECT * FROM players WHERE country='{country}'"
+    else:
+        query = "SELECT * FROM players"
+
+    players = conn.execute(query).fetchall()
+
+    return render_template("index.html", players=players)
+
+
+@app.route('/bid', methods=['POST'])
+def bid():
+
+    player_id = request.form['player_id']
+    bid_amount = int(request.form['bid'])
+
+    conn = engine.connect()
+
+    result = conn.execute(
+        f"SELECT current_price FROM players WHERE id={player_id}"
+    ).fetchone()
+
+    current_price = result[0]
+
+    if bid_amount > current_price:
+
+        conn.execute(
+            f"UPDATE players SET current_price={bid_amount} WHERE id={player_id}"
+        )
+
+        message = "Bid accepted!"
+
+    else:
+        message = "Bid must be higher than current price"
+
+    players = conn.execute("SELECT * FROM players").fetchall()
+
+    return render_template("index.html", players=players, message=message)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
